acc3b77ec6374b8db39e03c250798abdbfb978e5
Alex Schwartz <alexschwartz01@gmail.com>
2025-04-17 21:31:37 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,6 @@
-wget -qO - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor | sudo dd of=/etc/apt/keyrings/nodesource.gpg
-echo 'deb [ signed-by=/etc/apt/keyrings/nodesource.gpg ] https://deb.nodesource.com/node_22.x nodistro main' | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo dd of=/etc/apt/keyrings/packages.microsoft.gpg
-echo 'deb [ signed-by=/etc/apt/keyrings/packages.microsoft.gpg ] https://packages.microsoft.com/repos/code stable main' | sudo tee /etc/apt/sources.list.d/vscode.list
-
-sudo apt update
-sudo apt install nano code nodejs -y
-sudo apt purge vim --auto-remove -y
+sudo dnf install nodejs code -y
 mkdir -p ~/.local/bin ~/.local/lib
 npm config set prefix ~/.local
-
-sed -i "s/#alias ll='ls -l'/alias ll='ls -l'/" ~/.bashrc
-source ~/.profile
